2b11435c6e1ad63d7d017074156998b061edf248
IprymusO <malrej96@gmail.com>
2019-04-17 11:40:59 +0200

    lista zwiarząt

--- app/src/main/java/com/example/android_lab/MainActivity.java
+++ app/src/main/java/com/example/android_lab/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.android_lab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
 
-        //To Do coś tu zdo zrobienia
+public class MainActivity<target> extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] values = new String[] { "Pies",
+                "Kot", "Koń", "Gołąb", "Kruk", "Dzik", "Karp",
+                "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch" };
+
+        this.target = new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
+
+        this.adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, this.target);
+
+
+        ListView listview = (ListView) findViewById(
+                R.id.list );
+        listview.setAdapter(this.adapter);
+
+
     }
-}//komentarz
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
+
+
+
+    }
+
+
+
+//komentarz
